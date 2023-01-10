ae4cb8a7276013a4943a9f69846b9e19f361742d
nonamedm <nonamedm@naver.com>
2023-01-10 14:30:46 +0900

    푸시전송로직 추가

--- App.js
+++ App.js
@@ -18,15 +18,26 @@ export default class App extends React.Component {
             }
             token = (await Notifications.getExpoPushTokenAsync()).data;
             tokenValue = token;
-            setInterval(function(){
-                sendPush(tokenValue);
-            },5000);
+            // setInterval(function(){
+                // sendToken(tokenValue);
+            // },5000);
+            
+            if (Platform.OS === 'android') {
+                Notifications.setNotificationChannelAsync('default', {
+                    name: 'default',
+                    importance: Notifications.AndroidImportance.MAX,
+                    vibrationPattern: [0, 250, 250, 250],
+                    lightColor: '#FF231F7C',
+                });
+            }
+            sendToken(tokenValue);
+
             return token;
         }
         
         async function sendPush(tokenValue) {
 
-            console.log("전달받은 토큰 : ",tokenValue);
+            // console.log("전달받은 토큰 : ",tokenValue);
             await fetch('https://api.expo.dev/v2/push/send', {
                 method: 'POST',
                 cache: 'no-cache',
@@ -44,6 +55,18 @@ export default class App extends React.Component {
 
         }
 
-        return <WebView source={{ uri: 'https://www.kans.re.kr/origin_home/forum/admin/' }} style={{ marginTop: 20 }} />; 
+        async function sendToken(tokenValue) {
+
+            // console.log("전달받은 토큰 : ",tokenValue);
+            // console.log(data);
+            await fetch("http://www.kans.re.kr/origin_home/safety/bbs/save_token.php?token="+tokenValue)
+            .then(
+                // (response) => console.log(response)
+                )
+            .then((data) => console.log(data));
+
+        }
+
+        return <WebView source={{ uri: 'https://www.kans.re.kr/origin_home/safety/' }} style={{ marginTop: 20 }} />; 
    } 
 }
